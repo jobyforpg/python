b8309884e21749e8babe5e0560c8c455212f77a5
Joby George <formypg5599@gmail.com>
2025-11-07 11:22:08 +0530

    Update BANK.py

--- BANK.py
+++ BANK.py
@@ -1,6 +1,6 @@
 class Bank:
     def getdata(self,name,ac_type,ac_num,ac_bal):
-        selef.name=name
+        self.name=name
         self.ac_type=ac_type
         self.ac_num=ac_num
         self.ac_bal=ac_bal
@@ -12,22 +12,57 @@ class Bank:
         if(self.ac_bal<0):
             print("minim balance is requird")
         else:
+            self.ac_bal=self.ac_bal-witd
             
-            self.ac_bal=self.ac-witd
-            
-     def options(self):
+    def options(self):
+        while True:  
          print("1.Disply balance")
          print("2.withdraw")
          print("3.Deposit")
+         print("4. Exit")
          print("____________")
          ch=int(input("choose an option"))
-         switch(ch):
+         
+         match ch:
              case 1:
                  print("current balance:",self.ac_bal)
+                 
              case 2:
-                self.witd=int(print("Enter Amout"))
-                if(self.witd<100 and self.witd%=!100):
+               amount=int(input("Enter Amout"))
+
+               if  amount>self.ac_bal:
+                         print("insufficient balance")
+               elif amount<100 or amount%100!=0:
                     print("withdraw only multiple of 100 ")
-                
+              
+               else:
+                    self.witdraw(amout)
+                    print(amount,'withdraw')
+                    if(self.ac_bal>=0):
+                        
+                        print("current balance",  self.ac_bal)
+                    else:
+                        print(0,"balance")
+                        
+             case 3:
+                 amount=int(input("Enter Amout"))
+                 if amount<100 or amount%100!=0:
+                    print("deposit only multiple of 100 ")
+
+                 else:
+                    self.deposit(amount)
+                    print(amount,'Deposit')
+                    print("current balance",  self.ac_bal)
+
+
+             case 4: 
+                exit();
+B=Bank();
+B.getdata("joby","Saving",12345,0)
+B.options()
+
+
+ 
+                 
                  
                  
